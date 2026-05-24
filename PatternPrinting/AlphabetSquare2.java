bb53e62adcafa91315be4ae6060ffb2fe3231af3
Mahima-patidar441 <mahimapatidar776@gmail.com>
2026-05-24 21:38:39 +0530

    Add AlphabetSquare2 class for printing alphabet square

---
+++ PatternPrinting/AlphabetSquare2.java
@@ -0,0 +1,19 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class AlphabetSquare2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows : ");
+        int n = sc.nextInt();
+        char ch = 'A';
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= n ; j++) {
+                System.out.print(ch+" ");
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+}
