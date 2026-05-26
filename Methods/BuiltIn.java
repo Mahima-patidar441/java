3a4d850764e5bd89fa0fc26a952ad7a2a1ca4709
Mahima-patidar441 <mahimapatidar776@gmail.com>
2026-05-26 21:38:26 +0530

    Create BuiltIn.java

---
+++ Methods/BuiltIn.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class BuiltIn {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.cbrt(125));
+        System.out.println(Math.floor(2.3));
+        System.out.println(Math.ceil(3.9));
+        System.out.println(Math.min(5,3));
+        System.out.println(Math.max(67,34));
+        System.out.println(Math.pow(2,4));
+    }
+}
