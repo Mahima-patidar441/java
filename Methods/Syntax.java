2f54ceaf7fc5c5be33a2cbea8c618d07d09a74a9
Mahima-patidar441 <mahimapatidar776@gmail.com>
2026-05-26 21:39:53 +0530

    Add Syntax class with method to sum two numbers

---
+++ Methods/Syntax.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Syntax {
+    public static void Add(int a , int b ){
+        System.out.println("The sum of two number is : "+(a+b));
+    }
+    public static void main(String[] args) {
+
+        Add(5,4);
+    }
+}
