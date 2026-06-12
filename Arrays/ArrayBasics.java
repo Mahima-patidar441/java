e5bbb94f670333c8171f56b600439709c7288efb
Mahima Patidar <mahimapatidar776@gmail.com>
2026-06-12 23:02:57 +0530

    Create ArrayBasics.java

---
+++ Arrays/ArrayBasics.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class ArrayBasics {
+    public static void main(String[] args) {
+        int[] x = {2,52,67,9,93};
+        System.out.println(x[0]);
+        x[0] = 7;
+        System.out.println(x[0]);
+
+        int[] arr = new int[4];
+        arr[0] = 10 ;
+        arr[1] = 20 ;
+        arr[2] = 30 ;
+        arr[3] = 40 ;
+        System.out.println(arr[3]);
+
+
+
+    }
+}
