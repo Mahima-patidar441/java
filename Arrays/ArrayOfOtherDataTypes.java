162fb892f60c1bc82316dd2642dbf42d9228f3e8
Mahima Patidar <mahimapatidar776@gmail.com>
2026-06-12 23:04:11 +0530

    Create ArrayOfOtherDataTypes.java

---
+++ Arrays/ArrayOfOtherDataTypes.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class ArrayOfOtherDataTypes {
+    public static void main(String[] args) {
+        char[] arr = new char[8];
+        for (int i=0; i<arr.length;i++){
+            System.out.println(arr[i] +" ");
+        }
+        String[] x = {"sinu", "Tanu", "Pihu","Minu"};
+        for (int i=0; i<x.length;i++){
+            System.out.println(x[i] +" ");
+        }
+    }
+}
