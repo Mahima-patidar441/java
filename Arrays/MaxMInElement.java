ef183c443986e520b1552545120b95306dc063e4
Mahima Patidar <mahimapatidar776@gmail.com>
2026-06-12 23:05:18 +0530

    Add MaxMinElement class to find array extremes

---
+++ Arrays/MaxMInElement.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class MaxMInElement
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array : ");
+        for (int i = 0 ; i<n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+        for(int i=0 ; i<n ; i++){
+            if (arr[i] > max){
+                max = arr[i];
+            }
+            if (arr[i] < min){
+                min = arr[i];
+            }
+        }
+
+        System.out.println("The maximum element of the array is : "+max);
+        System.out.println("The miniimum element of the array is : "+min);
+
+
+    }
+}
