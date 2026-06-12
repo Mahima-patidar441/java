ed6b1f9ef4aa52e500e5ff82024c9c2f6468c751
Mahima Patidar <mahimapatidar776@gmail.com>
2026-06-12 23:04:35 +0530

    Add ForEachLoop class to print array elements

---
+++ Arrays/ForEachLoop.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] arr = { 5,8,9,6,2,1,2};
+        for (int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
