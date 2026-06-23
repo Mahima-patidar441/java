d73a48713b704b73c0681d1758f784d9141e2aa9
Mahima Patidar <mahimapatidar776@gmail.com>
2026-06-23 14:57:42 +0530

    Create StringsBasics.java

---
+++ Strings/StringsBasics.java
@@ -0,0 +1,19 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class StringsBasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        String s = sc.nextLine(); // can take input of multiple words
+//        System.out.println(s);
+//        String t = sc.next(); // can take only one word as a input
+//        System.out.println(t);
+
+//        String s1 = "Library";
+//        System.out.println(s1.charAt(4)); // to get the character at particular index
+//        System.out.println(s1.length()); // returns the length of the string
+
+
+    }
+}
