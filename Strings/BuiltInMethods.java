e86f272713c202130e21e4caf971bd434fb2e385
Mahima Patidar <mahimapatidar776@gmail.com>
2026-06-23 14:54:51 +0530

    Create BuiltInMethods class for string examples
    Added BuiltInMethods class demonstrating string operations.

---
+++ Strings/BuiltInMethods.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class BuiltInMethods {
+    public static void main(String[] args) {
+//        String s = " Mansi Modi";
+//        System.out.println(s.indexOf('o')); // returns index of any character
+//
+//        System.out.println(s.toLowerCase()); // returns strings in lowercase
+//
+//        System.out.println(s.contains("ans")); // returns true if these substrings is contained in the String
+
+        String a = "harmeet";
+        String b = "harshi";
+        System.out.println(a.compareTo(b));
+        System.out.println(a.concat(b));
+    }
+}
